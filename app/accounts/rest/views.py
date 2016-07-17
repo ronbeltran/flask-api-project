7a82a1802c9e2bbda4f9c27f716277fc14c2991c
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-07-17 19:12:28 +0800

    Setup dummy jwt protected view

--- app/accounts/rest/views.py
+++ app/accounts/rest/views.py
@@ -1,7 +1,16 @@
 from flask_restful import Resource
+from flask_jwt import current_identity
+
+from flask import g
+
+from ..jwt_helper import jwt_login_required
 
 
 class HelloView(Resource):
+    method_decorators = [jwt_login_required]
 
     def get(self):
+        print current_identity
+        print g.user_id
+        print g.user
         return {"message": "OK"}
