03882be4bef36a6dbd1a15c46dc055a90de46ff6
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-07-17 20:59:04 +0800

    Remove unused methods

--- app/accounts/jwt_helper.py
+++ app/accounts/jwt_helper.py
@@ -1,9 +1,6 @@
 from functools import wraps
-from datetime import datetime
-from datetime import timedelta
 
-import jwt
-from flask import current_app, g, request, jsonify
+from flask import g
 
 
 def jwt_authenticate(username_or_email, password):
@@ -19,49 +16,13 @@ def jwt_authenticate(username_or_email, password):
 def jwt_identity(payload):
     from .models import User
     user_id = payload["identity"]
-    return User.query.get(user_id)
+    user = User.query.get(user_id)
+    g.user = user
+    return user
 
 
-def create_jwt_token(user, days_expire=1, algorithm="RS256"):
-    payload = {
-        "user_id": user.id,
-        "username": user.username,
-        "email": user.email,
-        "iat": datetime.utcnow(),
-        "exp": datetime.utcnow() + timedelta(days=days_expire),
-    }
-    token = jwt.encode(payload, current_app.config["JWT_PUBLIC_KEY"], algorithm=algorithm)
-    return token.decode("unicode_escape")
-
-
-def parse_token(req):
-    auth_header = req.headers.get("Authorization").split()
-    if len(auth_header) != 2:
-        raise jwt.DecodeError("Authorization header is malformed.")
-    token = auth_header[1]
-    return jwt.decode(token, current_app.config["SECRET_KEY"])
-
-
-def jwt_login_required(fn):
+def dummy_decorator(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        from .models import User
-        if not request.headers.get("Authorization"):
-            response = jsonify(message="Missing authorization header")
-            response.status_code = 401
-            return response
-        try:
-            payload = parse_token(request)
-        except jwt.DecodeError as e:
-            response = jsonify(message=e.message or "Token invalid")
-            response.status_code = 401
-            return response
-        except jwt.ExpiredSignature:
-            response = jsonify(message="Token has expired")
-            response.status_code = 401
-            return response
-        print payload
-        g.user_id = payload["identity"]
-        g.user = User.query.get(payload["identity"])
         return fn(*args, **kwargs)
     return wrapper
