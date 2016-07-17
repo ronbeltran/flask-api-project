1355cab054a54953b0283390a7a48236f592b042
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-07-17 20:59:59 +0800

    Use the built-in jwt_required decorator

--- app/accounts/rest/views.py
+++ app/accounts/rest/views.py
@@ -1,16 +1,10 @@
-from flask_restful import Resource
-from flask_jwt import current_identity
-
 from flask import g
-
-from ..jwt_helper import jwt_login_required
+from flask_restful import Resource
+from flask_jwt import jwt_required
 
 
 class HelloView(Resource):
-    method_decorators = [jwt_login_required]
+    method_decorators = [jwt_required()]
 
     def get(self):
-        print current_identity
-        print g.user_id
-        print g.user
-        return {"message": "OK"}
+        return g.user.to_json()
